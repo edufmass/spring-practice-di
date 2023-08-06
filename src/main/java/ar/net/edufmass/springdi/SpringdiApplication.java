4ecbe5d3f598ba9fe1d4a44d1dd46f34d10dbf6b
edufmass <git@edufmass.ar>
2023-08-06 02:14:37 -0300

    Spring Context

--- src/main/java/ar/net/edufmass/springdi/SpringdiApplication.java
+++ src/main/java/ar/net/edufmass/springdi/SpringdiApplication.java
@@ -1,13 +1,19 @@
 package ar.net.edufmass.springdi;
 
+import ar.net.edufmass.springdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringdiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringdiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greetings = myController.sayHello();
+		System.out.println(greetings);
 	}
 
 }
