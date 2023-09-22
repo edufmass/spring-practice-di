d3d8f0f22db7f29353a41bf9e795a9504befb00d
edufmass <git@edufmass.ar>
2023-09-21 22:05:29 -0300

    Spring bean scope.

--- src/main/java/ar/net/edufmass/springdi/SpringdiApplication.java
+++ src/main/java/ar/net/edufmass/springdi/SpringdiApplication.java
@@ -1,6 +1,8 @@
 package ar.net.edufmass.springdi;
 
 import ar.net.edufmass.springdi.controllers.*;
+import ar.net.edufmass.springdi.services.PrototypeBean;
+import ar.net.edufmass.springdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -38,6 +40,18 @@ public class SpringdiApplication {
 		System.out.println("------ Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("------ Beans Scopes");
+
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
